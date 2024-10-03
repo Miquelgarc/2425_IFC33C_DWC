757b651ab6075a6d4e6362b987d409cbb7ba8cbb
“Miquel” <miquelgarcia@paucasesnovescifp.cat>
2024-10-03 18:18:13 +0200

    Començament del js

---
+++ piano/assets/js/piano.js
@@ -0,0 +1,15 @@
+var cerques = [];
+var partitures = [];
+function Nota(nom, sost) {
+    this.nom = nom;
+    this.sost = sost;
+}
+
+function addcerca(nom, tipus){
+    new Nota(nom, tipus)
+}
+
+function cercador() {
+    
+}
+
