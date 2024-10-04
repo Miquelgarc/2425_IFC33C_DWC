0def5304201a203d118799ce719d581b831f3843
“Miquel” <miquelgarcia@paucasesnovescifp.cat>
2024-10-04 20:05:09 +0200

    funcio cercador creada

--- piano/assets/js/piano.js
+++ piano/assets/js/piano.js
@@ -1,15 +1,33 @@
-var cerques = [];
-var partitures = [];
-function Nota(nom, sost) {
+var cerca = [];
+var partitures = [
+    { nom: "La Balanguera", notes: ["DO", "RE", "MI", "FA", "FA", "SOL", "SOL", "LA#", "LA#"] },
+    { nom: "Happy Birthday", notes: ["DO", "DO", "RE", "DO", "FA", "MI", "DO", "DO", "RE", "DO", "SOL", "FA"] }
+  ];
+function Nota(nom, tipus) {
     this.nom = nom;
-    this.sost = sost;
+    this.tipus = tipus;
 }
 
 function addcerca(nom, tipus){
-    new Nota(nom, tipus)
+    var novaNota = new Nota(nom, tipus);
+    cerca.push(novaNota.nom + novaNota.tipus);
 }
 
 function cercador() {
-    
+    var resultats = [];
+
+    partitures.forEach(partitura => {
+        var notesPartitura = partitura.notes.join(" ");
+        var notesCerca = cerca.join(" ");
+        if (notesPartitura.includes(notesCerca)) {
+            resultats.push(partitura.nom);
+        }
+    });
+
+    if (resultats.length>0) {
+        console.log("Partitures trobades: " + resultats.join(", "));
+    } else{
+        console.log("Sense resultats")
+    }
 }
 
