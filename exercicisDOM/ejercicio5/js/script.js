d440738143eee4d51903786b3e743477922c7cc7
“Miquel” <miquelgarcia@paucasesnovescifp.cat>
2024-10-31 16:35:27 +0100

    exercici5_DOM

---
+++ exercicisDOM/ejercicio5/js/script.js
@@ -0,0 +1,7 @@
+let encabezados = document.querySelectorAll("h1, h2");
+
+for (let encabezado of encabezados) {
+    encabezado.style.color = "red";
+    encabezado.style.fontSize = "30px";
+    encabezado.style.fontFamily = "Arial";
+}
