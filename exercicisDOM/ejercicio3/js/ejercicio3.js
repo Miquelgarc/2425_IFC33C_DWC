9fe8343b617ec52a1f82ab69f8126afad36d9b77
“Miquel” <miquelgarcia@paucasesnovescifp.cat>
2024-10-31 16:12:04 +0100

    exercici3_DOM

--- exercicisDOM/ejercicio3/js/ejercicio3.js
+++ exercicisDOM/ejercicio3/js/ejercicio3.js
@@ -1,1 +1,11 @@
 const nombres = ["Ana", "Luis", "María", "Carlos", "Sofía"];
+
+let ul = document.createElement("ul");
+let li;
+for (let nombre of nombres) {
+    li = document.createElement("li");
+    li.innerHTML = nombre;
+    ul.appendChild(li);
+}
+
+document.body.appendChild(ul);
