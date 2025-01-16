1205c63b5c7a7f3f5837e19c860878d22cf39f60
Miquel Garcia <miquelgarciallabres@paucasesnovescifp.cat>
2025-01-16 18:20:21 +0100

    Añadir archivo plantaEnvasado.js con estructura de contenedores y líquidos, eliminando plantaEnvasado.json

---
+++ ejercicios/ejerciciosJSON/plantaEnvasado.js
@@ -0,0 +1,61 @@
+let contenedores = [
+    {
+        "tipo": "grande",
+        "envases": [
+            {
+                "tipo": "grandeAncho",
+                "liquidos": [{
+                    "tipo": "alimentos",
+                    "contenido": ["leche", "agua", "aceite", "vinagre"]
+                }]
+
+            },
+            {
+                "tipo": "grandeAlto",
+                "liquidos": [{
+                    "tipo": "alimentos",
+                    "contenido": ["leche", "agua", "aceite", "vinagre"]
+                }]
+            }
+        ]
+    },
+    {
+        "tipo": "mediano",
+        "envases": [
+            {
+                "tipo": "medianoAncho",
+                "liquidos": [{
+                    "tipo": "alimentos",
+                    "contenido": ["leche", "agua", "aceite", "vinagre"]
+                }]
+            },
+            {
+                "tipo": "medianoAlto",
+                "liquidos": [{
+                    "tipo": "corrosivos",
+                    "contenido": ["amoniaco", "aguaras", "alcohol", "lejia"]
+                }]
+            }
+
+        ]
+    },
+    {
+        "tipo": "vertical",
+        "envases": [
+            {
+                "tipo": "grandeAlto",
+                "liquidos": [{
+                    "tipo": "alimentos",
+                    "contenido": ["leche", "agua", "aceite", "vinagre"]
+                }]
+            },
+            {
+                "tipo": "medianoAlto",
+                "liquidos": [{
+                    "tipo": "corrosivos",
+                    "contenido": ["amoniaco", "aguaras", "alcohol", "lejia"]
+                }]
+            }
+        ]
+    }
+]
